31f80553a21dffceeda1d11909738def41b93147
Christophe Larsonneur <clarsonneur@gmail.com>
2016-06-28 14:28:42 +0200

    Renamed go-forjj to goforjj

--- cmd/genflags/main.go
+++ cmd/genflags/main.go
@@ -37,7 +37,7 @@ const template_source=`package main
 
 import (
   "gopkg.in/alecthomas/kingpin.v2"
-  "github.hpe.com/christophe-larsonneur/go-forjj"
+  "github.hpe.com/christophe-larsonneur/goforjj"
   "os"
   "fmt"
 )
