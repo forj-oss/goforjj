5a1e60110dcca7fa4cbe9c09b704283cf2fa0ddb
Christophe Larsonneur <christophe.larsonneur@hpe.com>
2016-07-27 11:56:43 +0200

    Plugin enhance to support both shell and REST communications

---
+++ plugin-def.go
@@ -0,0 +1,51 @@
+package goforjj
+
+import (
+    "gopkg.in/yaml.v2"
+    "fmt"
+    "github.hpe.com/christophe-larsonneur/goforjj/trace"
+)
+
+// Load yaml raw data in YamlPlugin data structure
+func (p *PluginDef) PluginDefLoad(yaml_data []byte) (error) {
+    return yaml.Unmarshal([]byte(yaml_data), &p.Yaml)
+}
+
+// Initialize Plugin with Definition data.
+func (p *PluginDef) PluginInit(instance string) (error) {
+    gotrace.Trace("Initializing plugin instance '%s'\n", instance)
+    if p.Yaml.Name == "" {
+        return fmt.Errorf("Unable to initialize the plugin without Plugin definition.")
+    }
+    if err := p.def_runtime_context(); err != nil {
+        return err
+    }
+
+    // To define a unique container name based on workspace name.
+    p.docker.name = instance + "-" + p.Yaml.Name
+    gotrace.Trace("Service mode : %s\n", p.service)
+    return nil
+}
+
+func (p *PluginDef)def_runtime_context() (error) {
+    switch p.Yaml.Runtime.Service_type {
+    case "REST API": // REST API Service started as daemon
+        p.service = true
+
+    case "shell": // Shell/json process
+        p.service = false
+    default:
+        return fmt.Errorf("Error! Invalid '%s' service_type. Supports only 'REST API' and 'shell'. Use shell as default.", p.Yaml.Runtime.Service_type)
+    }
+    return nil
+}
+
+// Set plugin source path
+func (p *PluginDef) PluginSetSource(source_path string) {
+    p.Source_path = source_path
+}
+
+func (p *PluginDef) PluginSocketPath(path string) {
+    p.cmd.socket_path = path
+}
+
--- plugin-structs.go
+++ plugin-structs.go
@@ -1,9 +1,33 @@
 package goforjj
 
+import (
+    "github.com/parnurzeal/gorequest"
+    "net/url"
+)
+
 // Data stored about the driver
 type PluginDef struct {
-    Result *PluginResult // Json data structured returned.
-    Yaml YamlPlugin      // Yaml data definition
+    Result *PluginResult      // Json data structured returned.
+    Yaml YamlPlugin           // Yaml data definition
+    Source_path string        // Plugin source data
+    service bool              // True if the service is started as daemon
+    docker docker_container   // Define data to start the plugin as docker container
+    cmd cmd_data              // Define data to start the service process
+    req *gorequest.SuperAgent // REST API request
+    url *url.URL              // REST API url
+}
+
+type cmd_data struct {
+    command string       // Command to start
+    args []string        // Arrays of args to provide to the command
+    socket_path string   // Path to store the socket file
+    socket_file string
+}
+
+type docker_container struct {
+    name string
+    opts []string
+    socket_path string
 }
 
 // Following is created at create time or loaded from update/maintain
