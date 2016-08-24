cde90d22beb57c40a0fc9510924f58ed7546809d
Christophe Larsonneur <christophe.larsonneur@hpe.com>
2016-08-24 11:16:31 +0200

    Add http return code to let Do* functions to be able to set different http code than 200 or 422.

--- genapp/tmpl-rest-actions.go
+++ genapp/tmpl-rest-actions.go
@@ -19,32 +19,38 @@ import (
 // req_data contains the request data posted by forjj. Structure generated from '{{.Yaml.Name}}.yaml'.
 // ret_data contains the response structure to return back to forjj.
 //
-func DoCreate(w http.ResponseWriter, r *http.Request, req_data *CreateReq, ret_data *goforjj.PluginData) {
+// By default, if httpCode is not set (ie equal to 0), the function caller will set it to 422 in case of errors (error_message != "") or 200
+func DoCreate(w http.ResponseWriter, r *http.Request, req_data *CreateReq, ret_data *goforjj.PluginData) (httpCode int) {
 
     // This is where you shoud write your Update code. Following line is for Demo only.
     fmt.Fprintf(os.Stdout,"%#v\n", req_data)
 
+    return
 }
 
 // Do updating plugin task
 // req_data contains the request data posted by forjj. Structure generated from '{{.Yaml.Name}}.yaml'.
 // ret_data contains the response structure to return back to forjj.
 //
-func DoUpdate(w http.ResponseWriter, r *http.Request, req_data *UpdateReq, ret_data *goforjj.PluginData) {
+// By default, if httpCode is not set (ie equal to 0), the function caller will set it to 422 in case of errors (error_message != "") or 200
+func DoUpdate(w http.ResponseWriter, r *http.Request, req_data *UpdateReq, ret_data *goforjj.PluginData) (httpCode int){
 
     // This is where you shoud write your create code. Following line is for Demo only.
     fmt.Fprintf(os.Stdout,"%#v\n", req_data)
 
+    return
 }
 
 // Do maintaining plugin task
 // req_data contains the request data posted by forjj. Structure generated from '{{.Yaml.Name}}.yaml'.
 // ret_data contains the response structure to return back to forjj.
 //
-func DoMaintain(w http.ResponseWriter, r *http.Request, req_data *MaintainReq, ret_data *goforjj.PluginData) {
+// By default, if httpCode is not set (ie equal to 0), the function caller will set it to 422 in case of errors (error_message != "") or 200
+func DoMaintain(w http.ResponseWriter, r *http.Request, req_data *MaintainReq, ret_data *goforjj.PluginData) (httpCode int) {
 
     // This is where you shoud write your Update code. Following line is for Demo only.
     fmt.Fprintf(os.Stdout,"%#v\n", req_data)
 
+    return
 }
 `
