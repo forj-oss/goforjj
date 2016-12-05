1df12910d9b089e621d0e2e43203622fd84d0559
Christophe Larsonneur <clarsonneur@gmail.com>
2016-12-05 18:26:57 +0100

    Remove old shell capability.

--- plugin-actions.go
+++ plugin-actions.go
@@ -12,10 +12,7 @@ import (
 // If needed in a next iteration, we can move the API to match fully the RESTFul API with forjj objects/actions.
 // else start a shell or a container to get the json data.
 func (p *PluginDef) PluginRunAction(action string, d PluginReqData) (*PluginResult, error) {
-	if p.service {
-		return p.api_do(action, d)
-	}
-	return p.shell_do(action, d)
+	return p.api_do(action, d)
 }
 
 // api_do Internally execute the REST POST Call with parameters
@@ -50,35 +47,5 @@ func (p *PluginDef) api_do(action string, d PluginReqData) (*PluginResult, error
 		result.State_code = resp.StatusCode
 		return &result, fmt.Errorf("Plugin issue detected: %s", result.Data.ErrorMessage)
 	}
-	return &result, nil
-}
-
-// shell_do Internally execute the command with collection of parameters
-// returns the decoded data into predefined recognized PluginResult sructure
-func (p *PluginDef) shell_do(action string, d PluginReqData) (*PluginResult, error) {
-	cmd_args := []string{action}
-
-	for k, v := range d.Args {
-		cmd_args = append(cmd_args, "--"+k)
-		cmd_args = append(cmd_args, v)
-	}
-
-	if len(d.ReposData) > 0 {
-		if dj, err := json.Marshal(d.ReposData); err == nil {
-			cmd_args = append(cmd_args, "--data", string(dj))
-		}
-	}
-
-	body, err := cmd_run(cmd_args)
-	if err != nil {
-		return nil, err
-	}
-
-	var result PluginResult
-
-	if err := json.Unmarshal([]byte(body), &result); err != nil {
-		return nil, err
-	}
-
 	return &result, nil
 }
