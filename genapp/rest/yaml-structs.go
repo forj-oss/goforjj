ea4e2d3ca9dac2ff902c72d71d9013ed4c54df53
Christophe Larsonneur <clarsonneur@gmail.com>
2016-12-21 22:07:02 +0100

    Fix comment bad places

--- genapp/rest/yaml-structs.go
+++ genapp/rest/yaml-structs.go
@@ -23,8 +23,7 @@ type CreateArgReq struct {
 }
 
 // __MYPLUGIN: {{ range $Objectname, $ObjectOpts := .Yaml.Objects }}\
-type AppInstanceStruct struct {
-	// __MYPLUGIN: type {{ go_vars $Objectname}}InstanceStruct struct {
+type AppInstanceStruct struct { // __MYPLUGIN: type {{ go_vars $Objectname}}InstanceStruct struct {
 	// __MYPLUGIN: {{   range $ActionName, $Flags := object_tree $ObjectOpts }}\
 	Action AppActionStruct // __MYPLUGIN: 	{{ go_vars $ActionName}} {{ go_vars $Objectname}}{{ go_vars $ActionName}}Struct
 	// __MYPLUGIN: {{   end }}\
@@ -34,8 +33,7 @@ type AppInstanceStruct struct {
 
 // __MYPLUGIN: {{ range $Objectname, $ObjectOpts := .Yaml.Objects }}\
 // __MYPLUGIN: {{   range $ActionName, $Flags := object_tree $ObjectOpts }}\
-type AppActionStruct struct {
-	// __MYPLUGIN: type {{ go_vars $Objectname}}{{ go_vars $ActionName}}Struct struct {
+type AppActionStruct struct { // __MYPLUGIN: type {{ go_vars $Objectname}}{{ go_vars $ActionName}}Struct struct {
 	// __MYPLUGIN: {{     range $ParamName, $Opts := $Flags }}\
 	Param1 string // __MYPLUGIN: 	{{ go_vars $ParamName }} string // {{ $Opts.Help }}
 	// __MYPLUGIN: {{     end }}\
@@ -92,8 +90,7 @@ type MaintainArgReq struct {
 
 // __MYPLUGIN: {{ range $Objectname, $ObjectOpts := .Yaml.Objects }}\
 // __MYPLUGIN: {{   if object_has_secure $ObjectOpts }}\
-type AppMaintainStruct struct {
-	// __MYPLUGIN: type {{ go_vars $Objectname}}MaintainStruct struct {
+type AppMaintainStruct struct { // __MYPLUGIN: type {{ go_vars $Objectname}}MaintainStruct struct {
 	Setup struct {
 		// __MYPLUGIN: {{ range $ParamName, $Opts := $ObjectOpts.Flags }}\
 		// __MYPLUGIN: {{   if $Opts.Options.Secure }}\
