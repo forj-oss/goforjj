c497a1b25ac3f82979d0aa732113731458cbeaf9
Christophe Larsonneur <clarsonneur@gmail.com>
2017-06-02 15:44:26 +0200

    Use of-type to define a value to exchange between forjj and the plugin.

--- plugin-def-struct.go
+++ plugin-def-struct.go
@@ -60,7 +60,7 @@ type YamlFlag struct {
 	Help         string
 	FormatRegexp string   `yaml:"format-regexp"`
 	Actions      []string `yaml:"only-for-actions"`
-	List         bool
+	Type         string   `yaml:"of-type"`
 	FlagScope    string   `yaml:"flag-scope"`   // 'object' by default. Flag is not prefixed by instance name.
 												// 'instance' Flag is prefixed by instance name if certain condition.
 	FieldScope   string   `yaml:"fields-scope"` // 'object' by default. Means field is added at Object level.
