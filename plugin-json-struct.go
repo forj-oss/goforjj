7b964b3e997cb552080f2890a0993f00d23e2dc4
Chris <clarsonneur@gmail.com>
2017-07-19 17:28:38 +0200

    Simplify JSON payload

--- plugin-json-struct.go
+++ plugin-json-struct.go
@@ -8,6 +8,7 @@ type PluginRepo struct {
 	Exist         bool                           // True if the repo exist.
 	Remotes       map[string]PluginRepoRemoteUrl // k: remote name, v: remote url
 	BranchConnect map[string]string              // k: local branch name, v: remote/branch
+	Owner         string `json:",omitempty"`     // Owner name return by the plugin.
 }
 
 type PluginRepoRemoteUrl struct {
@@ -19,20 +20,9 @@ type PluginService struct {
 	Urls map[string]string
 }
 
-type PluginRepoData struct {
-	Templates []string          // RepoTemplates to apply
-	Title     string            // Repo Description
-	Users     map[string]string // Users and rights given
-	Groups    map[string]string // Groups and rights given
-	Flow      string            // Flow applied to the Repo
-	Instance  string            // Instance managing the upstream repo.
-	Options   map[string]string `yaml:",omitempty"` // More options forjj can send out as defined by the plugin itself.
-}
-
 // REST API json data
 type PluginData struct {
 	Repos         map[string]PluginRepo     `json:",omitempty"` // List of repository data
-	ReposData     map[string]PluginRepoData `json:",omitempty"` // Data associated to each Repository that the plugin should manage.
 	Services      PluginService             `json:",omitempty"` // web service url. ex: https://github.hpe.com
 	Status        string                    // Status message
 	CommitMessage string                    `json:",omitempty"` // Action commit message for Create/Update
