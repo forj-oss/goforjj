98f3947bc0edfb22f081dbc8b63f504bb0749f1b
Christophe Larsonneur <clarsonneur@gmail.com>
2018-04-12 13:52:46 +0200

    Support for Deploy repo destination

--- plugin-json.go
+++ plugin-json.go
@@ -1,8 +1,28 @@
 package goforjj
 
-func (d *PluginData) AddFile(file string) {
-	if d.Files == nil {
-		d.Files = make([]string, 0, 5)
+const (
+	source = "source"
+	deploy = "deploy"
+)
+
+// AddFile add a file the list of files Forjj will take care in GIT.
+//
+func (d *PluginData) AddFile(where, file string) {
+	if where == "" {
+		where = deploy
 	}
-	d.Files = append(d.Files, file)
+	if where != deploy && where != source {
+		where = deploy
+	}
+	if d.Files == nil {
+		d.Files = make(map[string][]string)
+	}
+	if v, found := d.Files[where]; !found {
+		v = make([]string, 1, 5)
+		v[0] = file
+		d.Files[where] = v
+	} else {
+		v = append(v, file)
+		d.Files[where] = v
+	}
 }
