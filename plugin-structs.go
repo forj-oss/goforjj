08bd63674873eae4bcd3358b1f0233c9bf51aeb6
Christophe Larsonneur <clarsonneur@gmail.com>
2018-05-19 20:56:11 +0200

    cleanup

--- plugin-structs.go
+++ plugin-structs.go
@@ -1,9 +1,5 @@
 package goforjj
 
-import (
-
-)
-
 type cmd_data struct {
 	command     string   // Command to start
 	args        []string // Arrays of args to provide to the command
@@ -19,12 +15,3 @@ type docker_container struct {
 	envs        map[string]byte
 }
 
-// Following is created at create time or loaded from update/maintain
-// File to define and store in the infra repository.
-type PluginsDefinition struct {
-	Plugins map[string]*Plugin   // Ex: plugins["upstream"] = "github"
-	Flow    string               // Ex: flow = "github-PR". This will connect all tools to provide a github PR flow Ready to start.
-}
-
-func init() {
-}
